ec920a5bf1d14209e066b747b62a87c5c77e9c30
Yeonju Jo <yjj0287@gmail.com>
2022-07-12 22:35:15 +0900

    chore: fix likeUp, ui

--- app/src/components/comment_list/comment_list.jsx
+++ app/src/components/comment_list/comment_list.jsx
@@ -3,16 +3,17 @@ import CommentItem from '../comment_item/comment_item';
 import styles from './comment_list.module.css';
 
 const CommentList = ({ comments }) => {
-    return (
-        <ul className={styles.box}>
-            {comments.map(comment => (
-                <CommentItem
-                    commentId={comment.commentid}
-                    comment={comment}
-                />
-            ))}
-        </ul>
-    );
+  return (
+    <ul className={styles.box}>
+      {comments.map(comment => (
+        <CommentItem
+          key={comment.commentid}
+          commentId={comment.commentid}
+          comment={comment}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default CommentList;
